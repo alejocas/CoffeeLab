729a5ecdd9c3baba3983c1665338926031e3a3f6
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-04-30 23:56:19 -0500

    creatre profile and home files views

---
+++ app/views/ProfilePage/ProfilePage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class ProfilePage extends Component {
+  render() {
+    return (
+      <Text>profile</Text>
+    );
+  }
+}
