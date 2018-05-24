b6fc7bab9c66e3fde2db0b23b92a231261dd3ece
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-05-24 00:52:44 -0500

    index para todas las paginas

---
+++ coffeeLab/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { HomePage } from "./home/home";
+export { LoginPage } from "./login/login";
+export { RegisterPage } from "./register/register";
+export { ResetPage } from "./reset/reset";
+
