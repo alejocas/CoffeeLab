f1244b02d61d325e784f37bc9c5e7b28ef328d6f
Angélica Arroyave <angelica.arroyavem@udea.edu.co>
2018-05-26 01:39:51 -0500

    Form validations was made

--- coffeeLab/src/pages/register/register.ts
+++ coffeeLab/src/pages/register/register.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage, LoginPage, ResetPage } from '../index';
 import { MenuController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 /**
@@ -18,15 +19,28 @@ import { MenuController } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private menuCtrl:MenuController) {
+  @ViewChild('signupSlider') signupSlider: any;
+  registerPag: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private menuCtrl: MenuController, public formBuilder: FormBuilder) {
+
     menuCtrl.enable(false, "menu");
+    this.registerPag = formBuilder.group({
+      usuario: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      contrasena: ['',Validators.compose([Validators.maxLength(30), Validators.required])],
+      nombre: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      apellidos: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      correo: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      numDocumento: ['',Validators.required ]
+    });
   }
-  
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
 
-  login(){
+  login() {
     this.navCtrl.push(LoginPage);
   }
 
