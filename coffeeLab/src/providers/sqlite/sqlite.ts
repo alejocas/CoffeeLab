d11d5a3942c9368c9cacd2c678e8903c52f2ece3
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-05-28 00:27:45 -0500

    servicio funcionando

--- coffeeLab/src/providers/sqlite/sqlite.ts
+++ coffeeLab/src/providers/sqlite/sqlite.ts
@@ -23,14 +23,15 @@ export class Sqlite {
 
       this.findByPk(object)
       .then(data =>{
-        console.log('validar',data)
-        let data1 = new Array(data)
-        if(data1.length == 0){
+
+        if(data){
+
           this.insert(object)
           .then(data => resolve(data))
           .catch(error => reject(error))
         }
         else{
+
           this.update(object)
           .then(data => resolve(data))
           .catch(error => reject(error))
@@ -39,14 +40,7 @@ export class Sqlite {
     });
     return promise;
   }
-  /*create(sql:string, params:any){
-    let promise = new Promise((resolve,reject)=>{
-      this.executeSQL(sql,params)
-      .then(data=>resolve(data))
-      .catch(error=>reject(error));
-    });
-    return promise;
-  }*/
+
 
   findAll(entity:any){
     let promise = new Promise((resolve,reject)=>{
@@ -110,31 +104,6 @@ export class Sqlite {
     return promise;
   }
 
-  /*create(sql:string, params:any){
-    let promise = new Promise((resolve,reject)=>{
-      this.sqlite.create({
-        name: 'data.db',
-        location: 'default' // the location field is required
-      })
-      .then((db) => {
-        this.db.executeSql(sql, params).then(data=>{
-          let tasks = [];
-          for (let index = 0; index < data.rows.length; index++) {
-            tasks.push( data.rows.item(index) );
-          }
-          resolve( tasks );
-        })
-        .catch(error=>{
-          reject(error);
-        });
-      })
-      .catch(error =>{
-        reject(error);
-      });
-    })
-    return promise;
-  }*/
-
   createTables(){
     let promise = new Promise((resolve, reject)=>{
       this.db.executeSql(TipoAbono.getSqlCreteTable(),{}).then(data=>{
@@ -157,94 +126,4 @@ export class Sqlite {
 
     return promise;
   }
-
-  /*delete(sql:string){
-    return this.db.executeSql(sql, {});
-  }
-
-  getAll(sql:string){
-
-    return this.db.executeSql(sql, [])
-    .then(response => {
-      let tasks = [];
-      for (let index = 0; index < response.rows.length; index++) {
-        tasks.push( response.rows.item(index) );
-      }
-      return Promise.resolve( tasks );
-    })
-    .catch(error => Promise.reject(error));
-  }
-
-  update(task: any){
-    let sql = 'UPDATE tasks SET title=?, completed=? WHERE id=?';
-    return this.db.executeSql(sql, [task.title, task.completed, task.id]);
-  }*/
-
 }
-
-  // executeSQL(sql:string){
-    
-  //   console.log(sql);
-
-  //   let promise = new Promise((resolve, reject) => {
-
-  //     this.sqlite.create({
-  //       name: 'data.db',
-  //       location: 'default'
-  //     })
-  //     .then((db: SQLiteObject) => {
-  //       db.executeSql(sql, {})
-  //         .then((data) => {
-  //           console.log('Executed SQL',data)
-  //           resolve(data);
-  //         })
-  //         .catch(e => {
-  //           console.log(e);
-  //           reject(e);
-  //         });
-  //     })
-  //     .catch(e => {
-  //       console.log(e);
-  //       reject(e);
-  //     });
-  //   });
-
-  //   return promise;
-  // }
-
-  // createDataBase(){
-
-  //   console.log("creando la base de datos");
-  //   /*let sql = TipoAbono.getSqlCreteTable() +
-  //                 TipoClima.getSqlCreteTable() +
-  //                 TipoDocumento.getSqlCreteTable() +
-  //                 TipoUsuario.getSqlCreteTable() +
-  //                 "INSERT INTO `TipoAbono`(`codigo`,`nombre`,`descripcion`) VALUES (1,'hola','');";
-  //   this.executeSQL(sql).then(data=>{
-  //     console.log(data);
-  //   },err=>{
-  //     console.log('error: ',err);
-  //   })
-  //   setTimeout(() => {
-  //     this.executeSQL(`SELECT * FROM TipoAbono;`)
-  //   }, 5000);*/
-  //   // this.executeSQL(TipoAbono.getSqlCreteTable() +
-  //   //               TipoClima.getSqlCreteTable() +
-  //   //               TipoDocumento.getSqlCreteTable() +
-  //   //               TipoUsuario.getSqlCreteTable()
-  //   //               );
-  //   this.executeSQL(TipoAbono.getSqlCreteTable()).then(data=>{
-  //     this.executeSQL("INSERT INTO `TipoAbono`(`codigo`,`nombre`,`descripcion`) VALUES (1,'hola','');").then(data=>{
-  //       this.executeSQL(`SELECT * FROM TipoAbono;`).then(data=>{
-  //         console.log(data)
-  //       });
-  //     });
-  //   })
-  //   this.executeSQL(TipoClima.getSqlCreteTable());
-  //   this.executeSQL(TipoDocumento.getSqlCreteTable());
-  //   this.executeSQL(TipoUsuario.getSqlCreteTable());
-   
-    
-  // }
-
-// }
