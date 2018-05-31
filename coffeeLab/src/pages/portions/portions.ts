b7091c50e6af54ebc44245da3177f56caeae5e4a
Angélica Arroyave <angelica.arroyavem@udea.edu.co>
2018-05-31 16:59:07 -0500

    Adding the alert for each lote wich has the riego, floración, recoleccion, abono, siembra... options

--- coffeeLab/src/pages/portions/portions.ts
+++ coffeeLab/src/pages/portions/portions.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, ActionSheetController } from 'ionic-angular';
 
 /**
  * Generated class for the PortionsPage page.
@@ -15,11 +15,58 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PortionsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public modalCtrl: ModalController, public actionSheetCtrl: ActionSheetController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PortionsPage');
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Acciones para el lote',
+      buttons: [
+        {
+          text: 'Riego',
+          icon: 'water',
+          handler: () => {
+            // this.openRiegoModal();
+          }
+        }, {
+          text: 'Suelo',
+          icon: 'globe',
+          handler: () => {
+            // this.openSueloModal();
+          }
+        }, {
+          text: 'Abono',
+          icon: 'leaf',
+          handler: () => {
+            // this.openAbonoModal();
+          }
+        }, {
+          text: 'Floración',
+          icon: 'flower',
+          handler: () => {
+            // this.openFloracionModal();
+          }
+        }, {
+          text: 'Recolección',
+          icon: 'nutrition',
+          handler: () => {
+           // this.openRecoleccionModal();
+          }
+        }, {
+          text: 'Plaga',
+          icon: 'bug',
+          handler: () => {
+            //this.openPlagaModal();
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
