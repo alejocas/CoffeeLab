e3ef66f729a7fa5bb1938294eb99e6a07c8b11a8
Santiago Gaviria <santi-skate@hotmail.com>
2018-06-01 01:14:39 -0500

    Comments and more comments

--- coffeeLab/src/pages/portion/portion.ts
+++ coffeeLab/src/pages/portion/portion.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {Finca, Lote} from '../../entities';
+import { Finca, Lote } from '../../entities';
 import { Sqlite } from '../../providers';
 import { Storage } from '@ionic/storage';
 
@@ -19,28 +19,41 @@ import { Storage } from '@ionic/storage';
 })
 export class PortionPage {
 
-  private fincas: Array<Finca>;
   private lote: Lote;
   private fincaPredeterminada: Finca;
 
+  /**
+   * Se inicializa el lote y a este se le asigna la finca que anteriormente
+   * fue elegida.
+   * @param navCtrl 
+   * @param navParams 
+   * @param db 
+   * @param storage 
+   */
   constructor(public navCtrl: NavController, public navParams: NavParams,
-  private db: Sqlite, private storage: Storage) {
+    private db: Sqlite, private storage: Storage) {
     this.storage.get('currentFinca')
-    .then(finca => {
-      this.fincaPredeterminada = new Finca(finca.codigo, finca.nombre, finca.tempPromedio, 
-        finca.altitud, finca.municipio, finca.tipoClima);
-    })
-    .catch(error => console.error(error));
-    this.lote = new Lote();
-    this.fincas = new Array<Finca>();
-
+      .then(finca => {
+        this.fincaPredeterminada = new Finca(finca.codigo, finca.nombre, finca.tempPromedio,
+          finca.altitud, finca.municipio, finca.tipoClima);
+      })
+      .catch(error => console.error(error));
+    if (this.fincaPredeterminada != null) {
+      this.lote = new Lote(null, this.fincaPredeterminada, null, null);
+    } else {
+      console.log("Elija una finca como predeterminada");
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PortionPage');
   }
 
-  savePortion(){
+
+  /**
+   * Almacena el lote en la finca predeterminada elegida previamente
+   */
+  savePortion() {
     this.db.save(this.lote);
     this.navCtrl.pop();
   }
