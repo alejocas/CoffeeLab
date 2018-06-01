148e7efb97f716b15d4af1dc1be81f5db3ef4a8b
Angélica Arroyave <angelica.arroyavem@udea.edu.co>
2018-05-31 22:35:14 -0500

    Revert "Portion view"

--- android/app/src/main/java/com/coffeelab/MainApplication.java
+++ android/app/src/main/java/com/coffeelab/MainApplication.java
@@ -11,35 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-import com.reactnativenavigation.NavigationApplication;
-
- public class MainApplication extends NavigationApplication {
-
-     @Override
-     public boolean isDebug() {
-         // Make sure you are using BuildConfig from your own application
-         return BuildConfig.DEBUG;
-     }
-
-     protected List<ReactPackage> getPackages() {
-         // Add additional packages you require here
-         // No need to add RnnPackage and MainReactPackage
-         return Arrays.<ReactPackage>asList(
-             // eg. new VectorIconsPackage()
-         );
-     }
-
-     @Override
-     public List<ReactPackage> createAdditionalReactPackages() {
-         return getPackages();
-     }
-
-     @Override
-      public String getJSMainModuleName() {
-          return "index";
-      }
- }
-/*public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -68,6 +40,6 @@ import com.reactnativenavigation.NavigationApplication;
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, false);
+    SoLoader.init(this, /* native exopackage */ false);
   }
-}*/
+}
--- coffeeLab/src/pages/index.ts
+++
@@ -1,9 +0,0 @@
-export { HomePage } from "./home/home";
-export { LoginPage } from "./login/login";
-export { RegisterPage } from "./register/register";
-export { ResetPage } from "./reset/reset";
-export { LandsPage } from "./lands/lands";
-export { LandPage } from "./land/land";
-export { PortionsPage } from "./portions/portions";
-export { ProfilePage } from "./profile/profile";
-export { ConfigPage } from "./config/config";
