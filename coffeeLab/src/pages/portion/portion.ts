a5fcc1c7ce224738dd7af3d3537506e64a97f3be
Santiago Gaviria <santi-skate@hotmail.com>
2018-06-01 00:34:57 -0500

    Still working in "portion"

--- coffeeLab/src/pages/portion/portion.ts
+++ coffeeLab/src/pages/portion/portion.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Finca, Lote} from '../../entities';
+import { Sqlite } from '../../providers';
+
 
 /**
  * Generated class for the PortionPage page.
@@ -15,11 +18,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PortionPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private fincas: Array<Finca>;
+  private lote: Lote;
+  private finca: Finca;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  private db: Sqlite) {
+    this.finca = new Finca();
+    this.lote = new Lote();
+    this.fincas = new Array<Finca>();
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PortionPage');
   }
+
+  fincaSeleccionada(finca){
+    this.finca = finca;
+  }
+  
 
 }
