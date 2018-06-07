fba8b33a5e2e026bd77743e8fcaa2eebba480c73
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-06-07 13:08:35 -0500

    lands

--- coffeeLab/src/app/app.component.ts
+++ coffeeLab/src/app/app.component.ts
@@ -24,7 +24,7 @@ import { isArray } from 'util';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
   
-  rootPage: any = HomePage; //RegisterPage; //default: LoginPage
+  rootPage: any = LoginPage; //RegisterPage; //default: LoginPage
 
   pages: Array<{title: string, component: any, icon: string}>;
 
